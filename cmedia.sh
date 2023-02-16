49d3bf0aed56207dcd67fec1c28b6fd034e02685
xaplhu <97669917+xaplhu@users.noreply.github.com>
2023-02-16 09:32:04 +0100

    Nuevo fichero y script añadidos

---
+++ cmedia.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+  echo "Debes especificar la ciudad como parámetro"
+  exit 1
+fi
+
+ciudad=$1
+total_consumo=0
+n_consumos=0
+
+while IFS=',' read -r ciudad_actual mes anio consumo; do
+  
+  if [[ $ciudad_actual == $ciudad ]]; then
+    total_consumo=$(( $total_consumo + $consumo ))
+    n_consumos=$(( $n_consumos + 1 ))
+  fi
+
+done < consumos.txt
+
+if (( $n_consumos > 0 )); then
+  media_consumo=$(echo "scale=2; $total_consumo / $n_consumos" | bc)
+  echo "La media de consumo en $ciudad es de: $media_consumo"
+else
+  echo "No se encontraron consumos para la ciudad especificada"
+fi
